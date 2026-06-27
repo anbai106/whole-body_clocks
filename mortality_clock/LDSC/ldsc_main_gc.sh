9d759dfdb201f2004644df81cbc1178624a6783e
anbai106 <anbai106@hotmail.com>
2026-06-27 11:04:33 -0400

    LDSC 3

--- mortality_clock/LDSC/ldsc_main_gc.sh
+++ mortality_clock/LDSC/ldsc_main_gc.sh
@@ -12,97 +12,79 @@ echo "Start training"
 
 ### let's loop all the MAEs for all the organs from our UKBB analysis + FinnGenn + PGC
 
-##### 11 ProtBAG
-#for bag in Reproductive_female Pulmonary Heart Brain Eye Hepatic Renal Reproductive_male Endocrine Immune Skin
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_Proteomics/gc/${bag}/${bag}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_ProtBAG_${bag}
-#done
-
-### 9 PhenoBAGs
-#for mae in Brain_age_gap Cardiovascular_age_gap Eye_age_gap Hepatic_age_gap Immune_age_gap Metabolic_age_gap Musculoskeletal_age_gap Pulmonary_age_gap Renal_age_gap
-#do
-#  stat_gz2="/cbica/home/wenju/Reproducibile_paper/BiologicalAge/output/GWAS/${mae}/gc/${mae}.sumstats.gz"
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_PhenoBAG_${mae}
-#done
-
-##### 5 MetBAG
-#for bag in Endocrine Digestive Hepatic Immune Metabolic
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/gc/${bag}/${bag}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_MetBAG_${bag}
-#done
-#
-##### with the 11 pan-disease MAE themselves
-#### brain
-#for mae in r1 r2 r3 r4 r5 r6
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/brain/${mae}/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_brain
-#done
-#
-#### eye
-#for mae in r1 r2 r3
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/eye/${mae}/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_eye
-#done
-#
-#### heart
-#for mae in r1 r2
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/heart/${mae}/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_heart
-#done
-#
-#### 9DNEs
-#for mae in AD_SurrealGAN_1 AD_SurrealGAN_2 ASD_1 ASD_2 ASD_3 LLD_1 LLD_2 SCZ_1 SCZ_2
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/DNE/output/GWAS/${mae}/gc/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
-#done
-#
-#### 3 multi-modal brain BAG
-#for mae in muse dmri_fullmetric_fa fmri
-#do
-#  if [ ${mae} == "dmri_fullmetric_fa" ]; then
-#    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/dmri.sumstats.gz"
-#  else
-#    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/${mae}.sumstats.gz"
-#  fi
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
-#done
-#
-#### FinnGen
-#summary_tsv='/cbica/home/wenju/Dataset/FinnGen/GWAS_summary_stats/summary_stats_R9_manifest_5000_cases.tsv'
-#variable=`awk '{print $1}' ${summary_tsv}`
-#for de in $variable
-#do
-#  if [ ${de} != "phenocode" ]; then
-#    stat_gz2="/cbica/home/wenju/Dataset/FinnGen/LDSC/finngen_R9_${de}.sumstats.gz"
-#    python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${de}
-#  fi
-#done
-#
-#### PGC
-#summary_tsv='/cbica/home/wenju/Dataset/PGC/PGC_MUTATE.tsv'
-#variable=`awk -F'\t' '{print $2}' ${summary_tsv}`
-#for de in $variable
-#do
-#  if [ ${de} != "Phenotype" ]; then
-#    stat_gz2="/cbica/home/wenju/Dataset/PGC/GWAS_summary_stats/Result/gc/ldsc/${de}.sumstats.gz"
-#    python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${de}
-#  fi
-#done
-
-### 327 raw metaboilites
-summary_tsv='/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/data/pheno_count_with_related_indi.txt'
-variable=`awk '{print $1}' ${summary_tsv}`
-for metabolite in $variable
+#### 11 ProtBAG
+for bag in Reproductive_female Pulmonary Heart Brain Eye Hepatic Renal Reproductive_male Endocrine Immune Skin
 do
-  if [ ${metabolite} != "Pheno" ]; then
-    stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/fastGWA/${metabolite}/${metabolite}.sumstats.gz
-    python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_metabolite_${metabolite}
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_Proteomics/gc/${bag}/${bag}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_ProtBAG_${bag}
+done
+
+#### 5 MetBAG
+for bag in Endocrine Digestive Hepatic Immune Metabolic
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/gc/${bag}/${bag}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_MetBAG_${bag}
+done
+
+#### with the 11 pan-disease MAE themselves
+### brain
+for mae in r1 r2 r3 r4 r5 r6
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/brain/${mae}/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_brain
+done
+
+### eye
+for mae in r1 r2 r3
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/eye/${mae}/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_eye
+done
+
+### heart
+for mae in r1 r2
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/heart/${mae}/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_heart
+done
+
+### 9DNEs
+for mae in AD_SurrealGAN_1 AD_SurrealGAN_2 ASD_1 ASD_2 ASD_3 LLD_1 LLD_2 SCZ_1 SCZ_2
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/DNE/output/GWAS/${mae}/gc/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
+done
+
+### 3 multi-modal brain BAG
+for mae in muse dmri_fullmetric_fa fmri
+do
+  if [ ${mae} == "dmri_fullmetric_fa" ]; then
+    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/dmri.sumstats.gz"
+  else
+    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/${mae}.sumstats.gz"
+  fi
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
+done
+
+### FinnGen
+summary_tsv='/cbica/home/wenju/Dataset/FinnGen/GWAS_summary_stats/summary_stats_R9_manifest_5000_cases.tsv'
+variable=`awk '{print $1}' ${summary_tsv}`
+for de in $variable
+do
+  if [ ${de} != "phenocode" ]; then
+    stat_gz2="/cbica/projects/MULTI/processed/FinnGen/LDSC/finngen_R9_${de}.sumstats.gz"
+    python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${de}
+  fi
+done
+
+### PGC
+summary_tsv='/cbica/home/wenju/Dataset/PGC/PGC_MUTATE.tsv'
+variable=`awk -F'\t' '{print $2}' ${summary_tsv}`
+for de in $variable
+do
+  if [ ${de} != "Phenotype" ]; then
+    stat_gz2="/cbica/projects/MULTI/processed/PGC/GWAS_summary_stats/Result/gc/ldsc/${de}.sumstats.gz"
+    python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${de}
   fi
 done
 
--- mortality_clock/LDSC/ldsc_main_gc_submit.sh
+++ mortality_clock/LDSC/ldsc_main_gc_submit.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 #SBATCH --partition=all
-#SBATCH --job-name=ArrayJob
-#SBATCH --array=0-4
+#SBATCH --job-name=LDSC_main_mortality_clock
+#SBATCH --array=0-21
 #SBATCH --mem-per-cpu=12G
 #SBATCH --time=0-05:59:00
-#SBATCH --output=/cbica/home/wenju/output/LDSC_%A_%a.out
-#SBATCH --error=/cbica/home/wenju/output/LDSC_%A_%a.err
+#SBATCH --output=/cbica/home/wenju/output/LDSC_main_%A_%a.out
+#SBATCH --error=/cbica/home/wenju/output/LDSC_main_%A_%a.err
 
-numbers=(Endocrine Digestive Hepatic Immune Metabolic)
-organ=${numbers[$SLURM_ARRAY_TASK_ID]}
+set -euo pipefail
 
-stat_gz1=/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/gc/${organ}/${organ}.sumstats.gz
-output_file=/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/gc/${organ}/${organ}_vs
-bash /cbica/home/wenju/Project/UKBB_NMR_metabolomics/LDSC/ldsc_main_gc.sh ${stat_gz1} ${output_file}
+base_dir="/cbica/home/wenju/Reproducibile_paper/WholeBodyClock/mortality_clock/fastGWA/output"
 
+# Collect all munged LDSC sumstats files in a stable sorted order
+mapfile -t stat_files < <(
+  find "${base_dir}" \
+    -mindepth 2 \
+    -maxdepth 2 \
+    -type f \
+    -name "organ_pheno_normalized_residualized.fastGWA.ldsc.sumstats.gz" \
+  | sort
+)
+
+# Sanity check
+n_files=${#stat_files[@]}
+echo "Found ${n_files} LDSC sumstats files."
+
+if [[ "${SLURM_ARRAY_TASK_ID}" -ge "${n_files}" ]]; then
+  echo "ERROR: SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID} but only ${n_files} files found."
+  exit 1
+fi
+
+stat_gz1="${stat_files[$SLURM_ARRAY_TASK_ID]}"
+
+organ_dir_path=$(dirname "${stat_gz1}")
+organ_dir=$(basename "${organ_dir_path}")
+organ="${organ_dir%_mortality_clock}"
+
+# Create LDSC output subfolder inside the corresponding organ-clock folder
+ldsc_out_dir="${organ_dir_path}/ldsc"
+mkdir -p "${ldsc_out_dir}"
+
+# Output prefix for LDSC main analysis
+output_file="${ldsc_out_dir}/${organ}_vs"
+
+echo "SLURM job ID: ${SLURM_JOB_ID}"
+echo "Array task ID: ${SLURM_ARRAY_TASK_ID}"
+echo "Organ clock: ${organ}"
+echo "Input sumstats: ${stat_gz1}"
+echo "LDSC output directory: ${ldsc_out_dir}"
+echo "Output prefix: ${output_file}"
+
+bash /cbica/home/wenju/Project/UKBB_NMR_metabolomics/LDSC/ldsc_main_gc.sh "${stat_gz1}" "${output_file}"
