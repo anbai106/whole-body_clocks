6a759b8a252f6d88837cdc3ef2293fa21573afad
anbai106 <anbai106@hotmail.com>
2026-06-29 22:05:45 -0400

    h2 3

--- mortality_clock/GCTA/submit_genotype_proteomics.sh
+++ mortality_clock/GCTA/submit_genotype_proteomics.sh
@@ -20,7 +20,7 @@ cd $output_dir
 keep="${input_dir}/${organ}_proteomics_mortality_clock/EPOCH_keep_for_fastgwa.txt"
 pheno="${input_dir}/${organ}_proteomics_mortality_clock/EPOCH_pheno_normalized_residualized_with_related_indi.phen"
 out_name=${organ}
-grm=/cbica/home/wenju/Reproducibile_paper/UKBB_Proteomics/fastGWA/GRM_GCTA/fastGWA_grm_sparse_0.05
+grm=/cbica/home/wenju/Reproducibile_paper/UKBB_Proteomics/h2/GRM_GCTA/gcta_h2_grm ## need rerun GRM for h2
 if [[ -f ${output_dir}/${organ}_proteomics_mortality_clock/${organ}.hsq ]]; then
     echo "GCTA has been run..."
     :
