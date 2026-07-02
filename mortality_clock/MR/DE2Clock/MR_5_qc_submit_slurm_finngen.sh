2aa7525df15997b29ca694d8d3385a847deaf3a4
anbai106 <anbai106@hotmail.com>
2026-07-02 15:08:25 -0400

    run MR Clock2DE QC 8

--- mortality_clock/MR/DE2Clock/MR_5_qc_submit_slurm_finngen.sh
+++ mortality_clock/MR/DE2Clock/MR_5_qc_submit_slurm_finngen.sh
@@ -8,20 +8,25 @@
 #SBATCH --error=/cbica/home/wenju/output/DE2Pheno_%A_%a.err
 
 ############################## END OF DEFAULT EMBEDDED SGE COMMANDS #######################
-### define input var
+### finngenfine input var
 
 module load R/4.3
 
 #### FinnGen
-output_dir=/cbica/projects/MULTI/processed/FinnGen/2SampleMR/FINNGEN2MRIBAG
+output_dir=/cbica/home/wenju/Reproducibile_paper/WholeBodyClock/mortality_clock/MR/DE2Clock/FinnGen
 mkdir -p $output_dir
-de_array=( $( awk '{print $1}' /cbica/projects/MULTI/processed/FinnGen/GWAS_summary_stats/summary_stats_R9_manifest_5000_cases.tsv | grep -v "phenocode" | xargs) )
-de=${de_array[$SLURM_ARRAY_TASK_ID]}
+finngen_array=( $( awk '{print $1}' /cbica/projects/MULTI/processed/FinnGen/GWAS_summary_stats/summary_stats_R9_manifest_5000_cases.tsv | grep -v "phenocofinngen" | xargs) )
+finngen=${finngen_array[$SLURM_ARRAY_TASK_ID]}
+
+### 7 MRI
 for organ in brain adipose heart kidney liver pancreas spleen
 do
-    mr_tsv=${output_dir}/MR_${de}_2_${organ}.tsv
+    output_dir_har=${output_dir}/${organ}_mri_mortality_clock/harmonization
+    output_dir_mr=${output_dir}/${organ}_mri_mortality_clock/MR
+    output_dir_qc=${output_dir}/${organ}_mri_mortality_clock/QC
+    mr_tsv=${output_dir_mr}/MR_${finngen}_2_${organ}.tsv
     if [ -f "${mr_tsv}" ]; then
-    p_value_thres=0.00023364485981308412 ### 0.05/214 DEs
+    p_value_thres=0.000234742 ### 0.05/N DEs
 
     # Debugging: Check p-values
     echo "Checking p-values in ${mr_tsv}..."
@@ -30,10 +35,64 @@ do
     significant_pval=$(awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | \
                        awk '{if ($1+0 < '"$p_value_thres"') print $1}' | head -n 1)
     if [ -n "$significant_pval" ]; then
-      echo "Run 2SampleMR QC from ${de} to ${organ}..."
-      Rscript /cbica/home/wenju/Project/AbdoImaging/MR/DE2BAG/MR_5_qc.R ${de} ${organ} ${output_dir}
+      echo "Run 2SampleMR QC from ${finngen} to ${organ}..."
+      Rscript /cbica/home/wenju/Project/whole-body_clocks/mortality_clock/MR/DE2Clock/MR_5_qc.R ${finngen} ${organ} ${output_dir_qc} ${output_dir_har} ${output_dir_mr}
     else
-      echo "Not significant for ${de}..."
+      echo "Not significant for ${finngen}..."
+    fi
+  else
+    echo "File ${mr_tsv} not found."
+  fi
+done
+
+### 11 proteomics
+for organ in Reproductive_female Pulmonary Heart Brain Eye Hepatic Renal Reproductive_male Endocrine Immune Skin
+do
+    output_dir_har=${output_dir}/${organ}_proteomics_mortality_clock/harmonization
+    output_dir_mr=${output_dir}/${organ}_proteomics_mortality_clock/MR
+    output_dir_qc=${output_dir}/${organ}_proteomics_mortality_clock/QC
+    mr_tsv=${output_dir_mr}/MR_${finngen}_2_${organ}.tsv
+    if [ -f "${mr_tsv}" ]; then
+    p_value_thres=0.000234742 ### 0.05/214 DEs
+
+    # Debugging: Check p-values
+    echo "Checking p-values in ${mr_tsv}..."
+    awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | head
+    # Extract all p-values in the 9th column and check if any are below the threshold
+    significant_pval=$(awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | \
+                       awk '{if ($1+0 < '"$p_value_thres"') print $1}' | head -n 1)
+    if [ -n "$significant_pval" ]; then
+      echo "Run 2SampleMR QC from ${finngen} to ${organ}..."
+      Rscript /cbica/home/wenju/Project/whole-body_clocks/mortality_clock/MR/DE2Clock/MR_5_qc.R ${finngen} ${organ} ${output_dir_qc} ${output_dir_har} ${output_dir_mr}
+    else
+      echo "Not significant for ${finngen}..."
+    fi
+  else
+    echo "File ${mr_tsv} not found."
+  fi
+done
+
+### 4 proteomics
+for organ in Endocrine Digestive Hepatic Immune
+do
+    output_dir_har=${output_dir}/${organ}_metabolomics_mortality_clock/harmonization
+    output_dir_mr=${output_dir}/${organ}_metabolomics_mortality_clock/MR
+    output_dir_qc=${output_dir}/${organ}_metabolomics_mortality_clock/QC
+    mr_tsv=${output_dir_mr}/MR_${finngen}_2_${organ}.tsv
+    if [ -f "${mr_tsv}" ]; then
+    p_value_thres=0.000234742 ### 0.05/214 DEs
+
+    # Debugging: Check p-values
+    echo "Checking p-values in ${mr_tsv}..."
+    awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | head
+    # Extract all p-values in the 9th column and check if any are below the threshold
+    significant_pval=$(awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | \
+                       awk '{if ($1+0 < '"$p_value_thres"') print $1}' | head -n 1)
+    if [ -n "$significant_pval" ]; then
+      echo "Run 2SampleMR QC from ${finngen} to ${organ}..."
+      Rscript /cbica/home/wenju/Project/whole-body_clocks/mortality_clock/MR/DE2Clock/MR_5_qc.R ${finngen} ${organ} ${output_dir_qc} ${output_dir_har} ${output_dir_mr}
+    else
+      echo "Not significant for ${finngen}..."
     fi
   else
     echo "File ${mr_tsv} not found."
--- mortality_clock/MR/DE2Clock/MR_5_qc_submit_slurm_pgc.sh
+++ mortality_clock/MR/DE2Clock/MR_5_qc_submit_slurm_pgc.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 #SBATCH --partition=all
-#SBATCH --job-name=DE2BAG
-#SBATCH --array=0-3
+#SBATCH --job-name=Clock2DE
+#SBATCH --array=0-21
 #SBATCH --mem-per-cpu=12G
-#SBATCH --output=/cbica/home/wenju/output/DE2Pheno_%A_%a.out
-#SBATCH --error=/cbica/home/wenju/output/DE2Pheno_%A_%a.err
+#SBATCH --time=0-05:59:00
+#SBATCH --output=/cbica/home/wenju/output/Clock2DE%A_%a.out
+#SBATCH --error=/cbica/home/wenju/output/Clock2DE%A_%a.err
 
 ############################## END OF DEFAULT EMBEDDED SGE COMMANDS #######################
-### define input var
+output_dir=/cbica/home/wenju/Reproducibile_paper/WholeBodyClock/mortality_clock/MR/DE2Clock/PGC
+numbers=(adipose_mri_mortality_clock Brain_proteomics_mortality_clock Endocrine_metabolomics_mortality_clock Eye_proteomics_mortality_clock Heart_proteomics_mortality_clock Hepatic_proteomics_mortality_clock Immune_proteomics_mortality_clock liver_mri_mortality_clock Pulmonary_proteomics_mortality_clock Reproductive_female_proteomics_mortality_clock Skin_proteomics_mortality_clock brain_mri_mortality_clock Digestive_metabolomics_mortality_clock Endocrine_proteomics_mortality_clock heart_mri_mortality_clock Hepatic_metabolomics_mortality_clock Immune_metabolomics_mortality_clock kidney_mri_mortality_clock pancreas_mri_mortality_clock Renal_proteomics_mortality_clock Reproductive_male_proteomics_mortality_clock spleen_mri_mortality_clock)
+organ=${numbers[$SLURM_ARRAY_TASK_ID]}
+organ_first="${organ%%_*}"
 
 module load R/4.3
 
-output_dir=/cbica/projects/MULTI/processed/PGC/GWAS_summary_stats/Result/2SampleMR/PGC2MRIBAG
-mkdir -p $output_dir
-de_array=( $( awk '{print $2}' /cbica/projects/MULTI/processed/PGC/PGC_MUTATE_MR.tsv | grep -v "Phenotype" | xargs) )
-de=${de_array[$SLURM_ARRAY_TASK_ID]}
-for organ in brain adipose heart kidney liver pancreas spleen
+### PGC
+for pgc in AD ADHD BIP SCZ
 do
-    mr_tsv=${output_dir}/MR_${de}_2_${organ}.tsv
+    output_dir_mr=${output_dir}/${organ}/MR
+    output_dir_qc=${output_dir}/${organ}/QC
+    output_dir_har=${output_dir}/${organ}/harmonization
+    mr_tsv=${output_dir_mr}/MR_${organ_first}_2_${pgc}.tsv
     if [ -f "${mr_tsv}" ]; then
-    p_value_thres=0.00023364485981308412 ### 0.05/214 DEs
+      p_value_thres=0.000234742 ### 0.05/N DEs
 
-    # Debugging: Check p-values
-    echo "Checking p-values in ${mr_tsv}..."
-    awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | head
-    # Extract all p-values in the 9th column and check if any are below the threshold
-    significant_pval=$(awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | \
-                       awk '{if ($1+0 < '"$p_value_thres"') print $1}' | head -n 1)
-    if [ -n "$significant_pval" ]; then
-      echo "Run 2SampleMR QC from ${de} to ${organ}..."
-      Rscript /cbica/home/wenju/Project/AbdoImaging/MR/DE2BAG/MR_5_qc.R ${de} ${organ} ${output_dir}
+      # Debugging: Check p-values
+      echo "Checking p-values in ${mr_tsv}..."
+      awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | head
+      # Extract all p-values in the 9th column and check if any are below the threshold
+      significant_pval=$(awk -F'\t' 'NR > 1 && $9 != "" {print $9}' "${mr_tsv}" | \
+                         awk '{if ($1+0 < '"$p_value_thres"') print $1}' | head -n 1)
+      if [ -n "$significant_pval" ]; then
+        echo "Run 2SampleMR QC from ${pgc} to ${organ_first}..."
+        Rscript /cbica/home/wenju/Project/AbdoImaging/MR/DE2BAG/MR_5_qc.R ${pgc} ${organ_first} ${output_dir_qc} ${output_dir_har} ${output_dir_mr}
+      else
+        echo "Not significant for ${pgc}..."
+      fi
     else
-      echo "Not significant for ${de}..."
+      echo "File ${mr_tsv} not found."
     fi
-  else
-    echo "File ${mr_tsv} not found."
-  fi
 done
