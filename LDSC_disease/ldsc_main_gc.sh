1871759fd2e6197aa8b64d9f82e8d1143f1a466e
anbai106 <anbai106@hotmail.com>
2026-07-11 09:10:28 -0400

    disease clock gc 2

--- LDSC_disease/ldsc_main_gc.sh
+++ LDSC_disease/ldsc_main_gc.sh
@@ -12,66 +12,66 @@ echo "Start training"
 
 ### let's loop all the MAEs for all the organs from our UKBB analysis + FinnGenn + PGC
 
-##### 11 ProtBAG
-#for bag in Reproductive_female Pulmonary Heart Brain Eye Hepatic Renal Reproductive_male Endocrine Immune Skin
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_Proteomics/gc/${bag}/${bag}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_ProtBAG_${bag}
-#done
-#
-##### 5 MetBAG
-#for bag in Endocrine Digestive Hepatic Immune Metabolic
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/gc/${bag}/${bag}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_MetBAG_${bag}
-#done
-#
-##### with the 11 pan-disease MAE themselves
-#### brain
-#for mae in r1 r2 r3 r4 r5 r6
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/brain/${mae}/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_brain
-#done
-#
-#### eye
-#for mae in r1 r2 r3
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/eye/${mae}/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_eye
-#done
-#
-#### heart
-#for mae in r1 r2
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/heart/${mae}/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_heart
-#done
-#
-#### 9DNEs
-#for mae in AD_SurrealGAN_1 AD_SurrealGAN_2 ASD_1 ASD_2 ASD_3 LLD_1 LLD_2 SCZ_1 SCZ_2
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/DNE/output/GWAS/${mae}/gc/${mae}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
-#done
-#
-#### 3 multi-modal brain BAG
-#for mae in muse dmri_fullmetric_fa fmri
-#do
-#  if [ ${mae} == "dmri_fullmetric_fa" ]; then
-#    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/dmri.sumstats.gz"
-#  else
-#    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/${mae}.sumstats.gz"
-#  fi
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
-#done
+#### 11 ProtBAG
+for bag in Reproductive_female Pulmonary Heart Brain Eye Hepatic Renal Reproductive_male Endocrine Immune Skin
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_Proteomics/gc/${bag}/${bag}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_ProtBAG_${bag}
+done
 
-###### 7 MRIBAGs
-#for bag in brain adipose heart kidney liver pancreas spleen
-#do
-#  stat_gz2=/cbica/home/wenju/Reproducibile_paper/AbdoImaging/gc/${bag}/${bag}.sumstats.gz
-#  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_MRIBAG_${bag}
-#done
+#### 5 MetBAG
+for bag in Endocrine Digestive Hepatic Immune Metabolic
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/UKBB_metabolomics/gc/${bag}/${bag}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_MetBAG_${bag}
+done
+
+#### with the 11 pan-disease MAE themselves
+### brain
+for mae in r1 r2 r3 r4 r5 r6
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/brain/${mae}/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_brain
+done
+
+### eye
+for mae in r1 r2 r3
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/eye/${mae}/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_eye
+done
+
+### heart
+for mae in r1 r2
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/Multiorgan_Subtype/output/gc/heart/${mae}/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}_heart
+done
+
+### 9DNEs
+for mae in AD_SurrealGAN_1 AD_SurrealGAN_2 ASD_1 ASD_2 ASD_3 LLD_1 LLD_2 SCZ_1 SCZ_2
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/DNE/output/GWAS/${mae}/gc/${mae}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
+done
+
+### 3 multi-modal brain BAG
+for mae in muse dmri_fullmetric_fa fmri
+do
+  if [ ${mae} == "dmri_fullmetric_fa" ]; then
+    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/dmri.sumstats.gz"
+  else
+    stat_gz2="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/GWAS/${mae}/output/gc/${mae}.sumstats.gz"
+  fi
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_${mae}
+done
+
+##### 7 MRIBAGs
+for bag in brain adipose heart kidney liver pancreas spleen
+do
+  stat_gz2=/cbica/home/wenju/Reproducibile_paper/AbdoImaging/gc/${bag}/${bag}.sumstats.gz
+  python /cbica/home/wenju/Project/ldsc/ldsc.py --rg ${stat_gz1},${stat_gz2} --ref-ld-chr ${ref_ld_chr} --w-ld-chr ${w_ld_chr} --out ${output_file}_MRIBAG_${bag}
+done
 
 ### FinnGen
 summary_tsv='/cbica/projects/MULTI/processed/FinnGen/GWAS_summary_stats/summary_stats_R9_manifest_5000_cases.tsv'
