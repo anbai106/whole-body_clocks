5873f9ede6b8d39ae97bbcf20cd94c59de935d01
anbai106 <anbai106@hotmail.com>
2026-08-14 20:24:06 -0400

    run BWAS for 48 DTI tracks 4

--- reslient_ager_bag_epoch/BWAS/Brain/FC/fit_logistic_submit.sh
+++ reslient_ager_bag_epoch/BWAS/Brain/FC/fit_logistic_submit.sh
@@ -1,240 +1,380 @@
 #!/bin/bash
 #SBATCH --partition=all
-#SBATCH --job-name=BWAS
-#SBATCH --array=0-210
+#SBATCH --job-name=CRA_CUA_FC
+#SBATCH --array=0-209
 #SBATCH --mem-per-cpu=12G
 #SBATCH --time=0-02:59:00
-#SBATCH --output=/cbica/home/wenju/output/BWAS_%A_%a.out
-#SBATCH --error=/cbica/home/wenju/output/BWAS_%A_%a.err
+#SBATCH --output=/cbica/home/wenju/output/CRA_CUA_FC_%A_%a.out
+#SBATCH --error=/cbica/home/wenju/output/CRA_CUA_FC_%A_%a.err
 
-### details of the 210 full matrics: https://pmc.ncbi.nlm.nih.gov/articles/PMC7280786/
+###############################################################################
+# Functional MRI brain-wide association:
+#   Brain-proteomics EPOCH-BAG candidate resilient agers (CRA)
+#   versus concordant unfavorable agers (CUA)
+#
+# Functional-connectivity features:
+#   f_1 ... f_210 from fmri_25_component.tsv
+#
+# Outcome coding:
+#   CRA = 1
+#   CUA = 0
+#
+# Positive IDP beta / OR > 1:
+#   higher functional-connectivity feature value is associated with greater
+#   odds of CRA.
+#
+# Negative IDP beta / OR < 1:
+#   higher functional-connectivity feature value is associated with greater
+#   odds of CUA.
+#
+# Primary categorical phenotype:
+#   p20, using CRA_p20 versus CUA_p20. The R script constructs this binary
+#   phenotype automatically from the resilience TSV.
+#
+# There are 210 FC features, so valid SLURM indices are 0-209.
+###############################################################################
+
+# -----------------------------------------------------------------------------
+# Functional MRI features
+# -----------------------------------------------------------------------------
 
 idp_array=(
-f_1
-f_2
-f_3
-f_4
-f_5
-f_6
-f_7
-f_8
-f_9
-f_10
-f_11
-f_12
-f_13
-f_14
-f_15
-f_16
-f_17
-f_18
-f_19
-f_20
-f_21
-f_22
-f_23
-f_24
-f_25
-f_26
-f_27
-f_28
-f_29
-f_30
-f_31
-f_32
-f_33
-f_34
-f_35
-f_36
-f_37
-f_38
-f_39
-f_40
-f_41
-f_42
-f_43
-f_44
-f_45
-f_46
-f_47
-f_48
-f_49
-f_50
-f_51
-f_52
-f_53
-f_54
-f_55
-f_56
-f_57
-f_58
-f_59
-f_60
-f_61
-f_62
-f_63
-f_64
-f_65
-f_66
-f_67
-f_68
-f_69
-f_70
-f_71
-f_72
-f_73
-f_74
-f_75
-f_76
-f_77
-f_78
-f_79
-f_80
-f_81
-f_82
-f_83
-f_84
-f_85
-f_86
-f_87
-f_88
-f_89
-f_90
-f_91
-f_92
-f_93
-f_94
-f_95
-f_96
-f_97
-f_98
-f_99
-f_100
-f_101
-f_102
-f_103
-f_104
-f_105
-f_106
-f_107
-f_108
-f_109
-f_110
-f_111
-f_112
-f_113
-f_114
-f_115
-f_116
-f_117
-f_118
-f_119
-f_120
-f_121
-f_122
-f_123
-f_124
-f_125
-f_126
-f_127
-f_128
-f_129
-f_130
-f_131
-f_132
-f_133
-f_134
-f_135
-f_136
-f_137
-f_138
-f_139
-f_140
-f_141
-f_142
-f_143
-f_144
-f_145
-f_146
-f_147
-f_148
-f_149
-f_150
-f_151
-f_152
-f_153
-f_154
-f_155
-f_156
-f_157
-f_158
-f_159
-f_160
-f_161
-f_162
-f_163
-f_164
-f_165
-f_166
-f_167
-f_168
-f_169
-f_170
-f_171
-f_172
-f_173
-f_174
-f_175
-f_176
-f_177
-f_178
-f_179
-f_180
-f_181
-f_182
-f_183
-f_184
-f_185
-f_186
-f_187
-f_188
-f_189
-f_190
-f_191
-f_192
-f_193
-f_194
-f_195
-f_196
-f_197
-f_198
-f_199
-f_200
-f_201
-f_202
-f_203
-f_204
-f_205
-f_206
-f_207
-f_208
-f_209
-f_210
+  "f_1"
+  "f_2"
+  "f_3"
+  "f_4"
+  "f_5"
+  "f_6"
+  "f_7"
+  "f_8"
+  "f_9"
+  "f_10"
+  "f_11"
+  "f_12"
+  "f_13"
+  "f_14"
+  "f_15"
+  "f_16"
+  "f_17"
+  "f_18"
+  "f_19"
+  "f_20"
+  "f_21"
+  "f_22"
+  "f_23"
+  "f_24"
+  "f_25"
+  "f_26"
+  "f_27"
+  "f_28"
+  "f_29"
+  "f_30"
+  "f_31"
+  "f_32"
+  "f_33"
+  "f_34"
+  "f_35"
+  "f_36"
+  "f_37"
+  "f_38"
+  "f_39"
+  "f_40"
+  "f_41"
+  "f_42"
+  "f_43"
+  "f_44"
+  "f_45"
+  "f_46"
+  "f_47"
+  "f_48"
+  "f_49"
+  "f_50"
+  "f_51"
+  "f_52"
+  "f_53"
+  "f_54"
+  "f_55"
+  "f_56"
+  "f_57"
+  "f_58"
+  "f_59"
+  "f_60"
+  "f_61"
+  "f_62"
+  "f_63"
+  "f_64"
+  "f_65"
+  "f_66"
+  "f_67"
+  "f_68"
+  "f_69"
+  "f_70"
+  "f_71"
+  "f_72"
+  "f_73"
+  "f_74"
+  "f_75"
+  "f_76"
+  "f_77"
+  "f_78"
+  "f_79"
+  "f_80"
+  "f_81"
+  "f_82"
+  "f_83"
+  "f_84"
+  "f_85"
+  "f_86"
+  "f_87"
+  "f_88"
+  "f_89"
+  "f_90"
+  "f_91"
+  "f_92"
+  "f_93"
+  "f_94"
+  "f_95"
+  "f_96"
+  "f_97"
+  "f_98"
+  "f_99"
+  "f_100"
+  "f_101"
+  "f_102"
+  "f_103"
+  "f_104"
+  "f_105"
+  "f_106"
+  "f_107"
+  "f_108"
+  "f_109"
+  "f_110"
+  "f_111"
+  "f_112"
+  "f_113"
+  "f_114"
+  "f_115"
+  "f_116"
+  "f_117"
+  "f_118"
+  "f_119"
+  "f_120"
+  "f_121"
+  "f_122"
+  "f_123"
+  "f_124"
+  "f_125"
+  "f_126"
+  "f_127"
+  "f_128"
+  "f_129"
+  "f_130"
+  "f_131"
+  "f_132"
+  "f_133"
+  "f_134"
+  "f_135"
+  "f_136"
+  "f_137"
+  "f_138"
+  "f_139"
+  "f_140"
+  "f_141"
+  "f_142"
+  "f_143"
+  "f_144"
+  "f_145"
+  "f_146"
+  "f_147"
+  "f_148"
+  "f_149"
+  "f_150"
+  "f_151"
+  "f_152"
+  "f_153"
+  "f_154"
+  "f_155"
+  "f_156"
+  "f_157"
+  "f_158"
+  "f_159"
+  "f_160"
+  "f_161"
+  "f_162"
+  "f_163"
+  "f_164"
+  "f_165"
+  "f_166"
+  "f_167"
+  "f_168"
+  "f_169"
+  "f_170"
+  "f_171"
+  "f_172"
+  "f_173"
+  "f_174"
+  "f_175"
+  "f_176"
+  "f_177"
+  "f_178"
+  "f_179"
+  "f_180"
+  "f_181"
+  "f_182"
+  "f_183"
+  "f_184"
+  "f_185"
+  "f_186"
+  "f_187"
+  "f_188"
+  "f_189"
+  "f_190"
+  "f_191"
+  "f_192"
+  "f_193"
+  "f_194"
+  "f_195"
+  "f_196"
+  "f_197"
+  "f_198"
+  "f_199"
+  "f_200"
+  "f_201"
+  "f_202"
+  "f_203"
+  "f_204"
+  "f_205"
+  "f_206"
+  "f_207"
+  "f_208"
+  "f_209"
+  "f_210"
 )
 
-
-idp=${idp_array[$SLURM_ARRAY_TASK_ID]}
-
-sleep_dir=/cbica/home/wenju/Reproducibile_paper/SleepAging/NSS/data
-idp_tsv=/cbica/home/wenju/Reproducibile_paper/BrainAge/data/imaging/fmri_25_component.tsv
-cov_tsv=/cbica/home/wenju/Reproducibile_paper/PRS_UKBB/prediction/data/UKBB_fullsample_covariate.csv
-output_dir=/cbica/home/wenju/Reproducibile_paper/SleepAging/NSS/BWAS/Brain/FC
-mkdir -p $output_dir
-
-output_file="${output_dir}/NSS_logistic_results_${idp}.tsv"
-if [ ! -f ${output_file} ]; then
-  echo "Run GAMLSS for: ${idp}..."
-  bash /cbica/home/wenju/Project/SleepAging/NSS/BWAS/Brain/FC/fit_logistic.sh ${sleep_dir} ${idp_tsv} ${output_dir} ${idp} ${cov_tsv}
-else
-  :
+if [[ -z "${SLURM_ARRAY_TASK_ID:-}" ]]; then
+  echo "ERROR: SLURM_ARRAY_TASK_ID is not set." >&2
+  exit 1
 fi
+
+if (( SLURM_ARRAY_TASK_ID < 0 || SLURM_ARRAY_TASK_ID >= ${#idp_array[@]} )); then
+  echo "ERROR: SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID} is outside 0-$((${#idp_array[@]} - 1))." >&2
+  exit 1
+fi
+
+idp="${idp_array[$SLURM_ARRAY_TASK_ID]}"
+
+# -----------------------------------------------------------------------------
+# Input/output paths
+# -----------------------------------------------------------------------------
+
+PHENOTYPE_TSV="/cbica/home/wenju/Reproducibile_paper/WholeBodyClock/Brain_proteomics_mortality_clock/brain_proteomics_BAG_EPOCH_discordance_resilience.tsv"
+
+IDP_TSV="/cbica/home/wenju/Reproducibile_paper/BrainAge/data/imaging/fmri_25_component.tsv"
+
+COV_TSV="/cbica/home/wenju/Reproducibile_paper/PRS_UKBB/prediction/data/UKBB_fullsample_covariate.csv"
+
+OUTPUT_DIR="/cbica/home/wenju/Reproducibile_paper/WholeBodyClock/mortality_clock/BWAS/Brain_proteomics_EPOCH_BAG_resilience/CRA_vs_CUA/FC"
+
+# Keep the same generalized R script.
+R_SCRIPT="/cbica/home/wenju/Project/whole-body_clocks/reslient_ager_bag_epoch/BWAS/fit_logistic.R"
+
+mkdir -p "${OUTPUT_DIR}"
+mkdir -p "/cbica/home/wenju/output"
+
+# -----------------------------------------------------------------------------
+# Phenotype definition
+# -----------------------------------------------------------------------------
+
+PHENOTYPE_COL="aging_phenotype_p20"
+CASE_LABEL="CRA"
+CONTROL_LABEL="CUA"
+
+# -----------------------------------------------------------------------------
+# Covariates
+# -----------------------------------------------------------------------------
+#
+# Primary model:
+#   CRA vs CUA ~ FC_IDP_z + age_at_imaging + sex + Brain_ProtBAG_z
+#
+# age_at_imaging, sex, and Brain_ProtBAG_z are read from the resilience TSV.
+#
+# IMPORTANT FOR fMRI:
+# Motion and imaging-quality covariates can materially affect functional
+# connectivity. If fmri_25_component.tsv contains appropriate motion/QC
+# variables, add their exact column names to IDP_COVARIATES, separated by
+# commas, for example:
+#
+#   IDP_COVARIATES="mean_framewise_displacement,another_qc_variable"
+#
+# Do not invent variable names. Leave this empty unless those exact columns
+# are present in the imaging TSV.
+# -----------------------------------------------------------------------------
+
+PHENOTYPE_COVARIATES="age_at_imaging,sex,Brain_ProtBAG_z"
+
+# Optional general covariates read from COV_TSV.
+COVARIATES=""
+
+# Optional fMRI-specific motion / imaging-QC covariates read from IDP_TSV.
+IDP_COVARIATES=""
+
+# Categorical covariates included in the model.
+FACTOR_COVARIATES="sex"
+
+# -----------------------------------------------------------------------------
+# QC
+# -----------------------------------------------------------------------------
+
+OUTLIER_SD="4"
+MIN_TOTAL_N="100"
+MIN_PER_GROUP="30"
+
+# -----------------------------------------------------------------------------
+# R environment
+# -----------------------------------------------------------------------------
+
+module load R/4.2.2
+
+# -----------------------------------------------------------------------------
+# Run one functional-connectivity feature
+# -----------------------------------------------------------------------------
+
+comparison="${CASE_LABEL}_vs_${CONTROL_LABEL}"
+output_file="${OUTPUT_DIR}/${comparison}_logistic_results_${idp}.tsv"
+
+echo "============================================================"
+echo "CRA/CUA functional MRI association"
+echo "============================================================"
+echo "SLURM task:       ${SLURM_ARRAY_TASK_ID} / $((${#idp_array[@]} - 1))"
+echo "IDP:              ${idp}"
+echo "Phenotype column: ${PHENOTYPE_COL}"
+echo "Case:             ${CASE_LABEL} = 1"
+echo "Control:          ${CONTROL_LABEL} = 0"
+echo "Output:           ${output_file}"
+echo "============================================================"
+
+if [[ -s "${output_file}" ]]; then
+  echo "Result already exists; skipping:"
+  echo "  ${output_file}"
+  exit 0
+fi
+
+Rscript "${R_SCRIPT}" \
+  "${PHENOTYPE_TSV}" \
+  "${IDP_TSV}" \
+  "${COV_TSV}" \
+  "${OUTPUT_DIR}" \
+  "${idp}" \
+  "${PHENOTYPE_COL}" \
+  "${CASE_LABEL}" \
+  "${CONTROL_LABEL}" \
+  "${COVARIATES}" \
+  "${FACTOR_COVARIATES}" \
+  "${PHENOTYPE_COVARIATES}" \
+  "${IDP_COVARIATES}" \
+  "${OUTLIER_SD}" \
+  "${MIN_TOTAL_N}" \
+  "${MIN_PER_GROUP}"
+
+echo "Finished:"
+echo "  ${idp}"
--- reslient_ager_bag_epoch/BWAS/Brain/T1/fit_logistic_submit.sh
+++ reslient_ager_bag_epoch/BWAS/Brain/T1/fit_logistic_submit.sh
@@ -1,26 +1,281 @@
 #!/bin/bash
 #SBATCH --partition=all
-#SBATCH --job-name=BWAS
-#SBATCH --array=0-119
+#SBATCH --job-name=CRA_CUA_T1
+#SBATCH --array=0-118
 #SBATCH --mem-per-cpu=12G
 #SBATCH --time=0-02:59:00
-#SBATCH --output=/cbica/home/wenju/output/BWAS_%A_%a.out
-#SBATCH --error=/cbica/home/wenju/output/BWAS_%A_%a.err
+#SBATCH --output=/cbica/home/wenju/output/CRA_CUA_T1_%A_%a.out
+#SBATCH --error=/cbica/home/wenju/output/CRA_CUA_T1_%A_%a.err
 
+###############################################################################
+# T1 gray-matter brain-wide association:
+#   Brain-proteomics EPOCH-BAG candidate resilient agers (CRA)
+#   versus concordant unfavorable agers (CUA)
+#
+# Outcome coding:
+#   CRA = 1
+#   CUA = 0
+#
+# Positive IDP beta / OR > 1:
+#   higher regional gray-matter volume is associated with greater odds of CRA.
+#
+# Negative IDP beta / OR < 1:
+#   higher regional gray-matter volume is associated with greater odds of CUA.
+#
+# Primary categorical phenotype:
+#   p20, using CRA_p20 versus CUA_p20. The R script constructs this binary
+#   phenotype automatically from the resilience TSV.
+###############################################################################
 
-idp_array=( MUSE_Volume_23 MUSE_Volume_30 MUSE_Volume_31 MUSE_Volume_32 MUSE_Volume_36 MUSE_Volume_37 MUSE_Volume_38 MUSE_Volume_39 MUSE_Volume_47 MUSE_Volume_48 MUSE_Volume_55 MUSE_Volume_56 MUSE_Volume_57 MUSE_Volume_58 MUSE_Volume_59 MUSE_Volume_60 MUSE_Volume_71 MUSE_Volume_72 MUSE_Volume_73 MUSE_Volume_75 MUSE_Volume_76 MUSE_Volume_100 MUSE_Volume_101 MUSE_Volume_102 MUSE_Volume_103 MUSE_Volume_104 MUSE_Volume_105 MUSE_Volume_106 MUSE_Volume_107 MUSE_Volume_108 MUSE_Volume_109 MUSE_Volume_112 MUSE_Volume_113 MUSE_Volume_114 MUSE_Volume_115 MUSE_Volume_116 MUSE_Volume_117 MUSE_Volume_118 MUSE_Volume_119 MUSE_Volume_120 MUSE_Volume_121 MUSE_Volume_122 MUSE_Volume_123 MUSE_Volume_124 MUSE_Volume_125 MUSE_Volume_128 MUSE_Volume_129 MUSE_Volume_132 MUSE_Volume_133 MUSE_Volume_134 MUSE_Volume_135 MUSE_Volume_136 MUSE_Volume_137 MUSE_Volume_138 MUSE_Volume_139 MUSE_Volume_140 MUSE_Volume_141 MUSE_Volume_142 MUSE_Volume_143 MUSE_Volume_144 MUSE_Volume_145 MUSE_Volume_146 MUSE_Volume_147 MUSE_Volume_148 MUSE_Volume_149 MUSE_Volume_150 MUSE_Volume_151 MUSE_Volume_152 MUSE_Volume_153 MUSE_Volume_154 MUSE_Volume_155 MUSE_Volume_156 MUSE_Volume_157 MUSE_Volume_160 MUSE_Volume_161 MUSE_Volume_162 MUSE_Volume_163 MUSE_Volume_164 MUSE_Volume_165 MUSE_Volume_166 MUSE_Volume_167 MUSE_Volume_168 MUSE_Volume_169 MUSE_Volume_170 MUSE_Volume_171 MUSE_Volume_172 MUSE_Volume_173 MUSE_Volume_174 MUSE_Volume_175 MUSE_Volume_176 MUSE_Volume_177 MUSE_Volume_178 MUSE_Volume_179 MUSE_Volume_180 MUSE_Volume_181 MUSE_Volume_182 MUSE_Volume_183 MUSE_Volume_184 MUSE_Volume_185 MUSE_Volume_186 MUSE_Volume_187 MUSE_Volume_190 MUSE_Volume_191 MUSE_Volume_192 MUSE_Volume_193 MUSE_Volume_194 MUSE_Volume_195 MUSE_Volume_196 MUSE_Volume_197 MUSE_Volume_198 MUSE_Volume_199 MUSE_Volume_200 MUSE_Volume_201 MUSE_Volume_202 MUSE_Volume_203 MUSE_Volume_204 MUSE_Volume_205 MUSE_Volume_206 MUSE_Volume_207 )
-idp=${idp_array[$SLURM_ARRAY_TASK_ID]}
+# -----------------------------------------------------------------------------
+# T1 MUSE gray-matter IDPs
+# -----------------------------------------------------------------------------
 
-sleep_dir=/cbica/home/wenju/Reproducibile_paper/SleepAging/NSS/data
-idp_tsv=/cbica/home/wenju/Reproducibile_paper/BrainAge/data/imaging/T1_MUSE_GM.tsv
-cov_tsv=/cbica/home/wenju/Reproducibile_paper/PRS_UKBB/prediction/data/UKBB_fullsample_covariate.csv
-output_dir=/cbica/home/wenju/Reproducibile_paper/SleepAging/NSS/BWAS/Brain/T1
-mkdir -p $output_dir
+idp_array=(
+  "MUSE_Volume_23"
+  "MUSE_Volume_30"
+  "MUSE_Volume_31"
+  "MUSE_Volume_32"
+  "MUSE_Volume_36"
+  "MUSE_Volume_37"
+  "MUSE_Volume_38"
+  "MUSE_Volume_39"
+  "MUSE_Volume_47"
+  "MUSE_Volume_48"
+  "MUSE_Volume_55"
+  "MUSE_Volume_56"
+  "MUSE_Volume_57"
+  "MUSE_Volume_58"
+  "MUSE_Volume_59"
+  "MUSE_Volume_60"
+  "MUSE_Volume_71"
+  "MUSE_Volume_72"
+  "MUSE_Volume_73"
+  "MUSE_Volume_75"
+  "MUSE_Volume_76"
+  "MUSE_Volume_100"
+  "MUSE_Volume_101"
+  "MUSE_Volume_102"
+  "MUSE_Volume_103"
+  "MUSE_Volume_104"
+  "MUSE_Volume_105"
+  "MUSE_Volume_106"
+  "MUSE_Volume_107"
+  "MUSE_Volume_108"
+  "MUSE_Volume_109"
+  "MUSE_Volume_112"
+  "MUSE_Volume_113"
+  "MUSE_Volume_114"
+  "MUSE_Volume_115"
+  "MUSE_Volume_116"
+  "MUSE_Volume_117"
+  "MUSE_Volume_118"
+  "MUSE_Volume_119"
+  "MUSE_Volume_120"
+  "MUSE_Volume_121"
+  "MUSE_Volume_122"
+  "MUSE_Volume_123"
+  "MUSE_Volume_124"
+  "MUSE_Volume_125"
+  "MUSE_Volume_128"
+  "MUSE_Volume_129"
+  "MUSE_Volume_132"
+  "MUSE_Volume_133"
+  "MUSE_Volume_134"
+  "MUSE_Volume_135"
+  "MUSE_Volume_136"
+  "MUSE_Volume_137"
+  "MUSE_Volume_138"
+  "MUSE_Volume_139"
+  "MUSE_Volume_140"
+  "MUSE_Volume_141"
+  "MUSE_Volume_142"
+  "MUSE_Volume_143"
+  "MUSE_Volume_144"
+  "MUSE_Volume_145"
+  "MUSE_Volume_146"
+  "MUSE_Volume_147"
+  "MUSE_Volume_148"
+  "MUSE_Volume_149"
+  "MUSE_Volume_150"
+  "MUSE_Volume_151"
+  "MUSE_Volume_152"
+  "MUSE_Volume_153"
+  "MUSE_Volume_154"
+  "MUSE_Volume_155"
+  "MUSE_Volume_156"
+  "MUSE_Volume_157"
+  "MUSE_Volume_160"
+  "MUSE_Volume_161"
+  "MUSE_Volume_162"
+  "MUSE_Volume_163"
+  "MUSE_Volume_164"
+  "MUSE_Volume_165"
+  "MUSE_Volume_166"
+  "MUSE_Volume_167"
+  "MUSE_Volume_168"
+  "MUSE_Volume_169"
+  "MUSE_Volume_170"
+  "MUSE_Volume_171"
+  "MUSE_Volume_172"
+  "MUSE_Volume_173"
+  "MUSE_Volume_174"
+  "MUSE_Volume_175"
+  "MUSE_Volume_176"
+  "MUSE_Volume_177"
+  "MUSE_Volume_178"
+  "MUSE_Volume_179"
+  "MUSE_Volume_180"
+  "MUSE_Volume_181"
+  "MUSE_Volume_182"
+  "MUSE_Volume_183"
+  "MUSE_Volume_184"
+  "MUSE_Volume_185"
+  "MUSE_Volume_186"
+  "MUSE_Volume_187"
+  "MUSE_Volume_190"
+  "MUSE_Volume_191"
+  "MUSE_Volume_192"
+  "MUSE_Volume_193"
+  "MUSE_Volume_194"
+  "MUSE_Volume_195"
+  "MUSE_Volume_196"
+  "MUSE_Volume_197"
+  "MUSE_Volume_198"
+  "MUSE_Volume_199"
+  "MUSE_Volume_200"
+  "MUSE_Volume_201"
+  "MUSE_Volume_202"
+  "MUSE_Volume_203"
+  "MUSE_Volume_204"
+  "MUSE_Volume_205"
+  "MUSE_Volume_206"
+  "MUSE_Volume_207"
+)
 
-output_file="${output_dir}/NSS_logistic_results_${idp}.tsv"
-if [ ! -f ${output_file} ]; then
-  echo "Run GAMLSS for: ${idp}..."
-  bash /cbica/home/wenju/Project/SleepAging/NSS/BWAS/Brain/T1/fit_logistic.sh ${sleep_dir} ${idp_tsv} ${output_dir} ${idp} ${cov_tsv}
-else
-  :
+if [[ -z "${SLURM_ARRAY_TASK_ID:-}" ]]; then
+  echo "ERROR: SLURM_ARRAY_TASK_ID is not set." >&2
+  exit 1
 fi
+
+if (( SLURM_ARRAY_TASK_ID < 0 || SLURM_ARRAY_TASK_ID >= ${#idp_array[@]} )); then
+  echo "ERROR: SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID} is outside 0-$((${#idp_array[@]} - 1))." >&2
+  exit 1
+fi
+
+idp="${idp_array[$SLURM_ARRAY_TASK_ID]}"
+
+# -----------------------------------------------------------------------------
+# Input/output paths
+# -----------------------------------------------------------------------------
+
+PHENOTYPE_TSV="/cbica/home/wenju/Reproducibile_paper/WholeBodyClock/Brain_proteomics_mortality_clock/brain_proteomics_BAG_EPOCH_discordance_resilience.tsv"
+
+IDP_TSV="/cbica/home/wenju/Reproducibile_paper/BrainAge/data/imaging/T1_MUSE_GM.tsv"
+
+COV_TSV="/cbica/home/wenju/Reproducibile_paper/PRS_UKBB/prediction/data/UKBB_fullsample_covariate.csv"
+
+OUTPUT_DIR="/cbica/home/wenju/Reproducibile_paper/WholeBodyClock/mortality_clock/BWAS/Brain_proteomics_EPOCH_BAG_resilience/CRA_vs_CUA/T1"
+
+# Keep the R script name exactly as requested.
+R_SCRIPT="/cbica/home/wenju/Project/whole-body_clocks/reslient_ager_bag_epoch/BWAS/fit_logistic.R"
+
+mkdir -p "${OUTPUT_DIR}"
+mkdir -p "/cbica/home/wenju/output"
+
+# -----------------------------------------------------------------------------
+# Phenotype definition
+# -----------------------------------------------------------------------------
+
+PHENOTYPE_COL="aging_phenotype_p20"
+CASE_LABEL="CRA"
+CONTROL_LABEL="CUA"
+
+# -----------------------------------------------------------------------------
+# Covariates
+# -----------------------------------------------------------------------------
+#
+# Primary model:
+#   CRA vs CUA ~ T1_IDP_z + age_at_imaging + sex + Brain_ProtBAG_z
+#
+# age_at_imaging, sex, and Brain_ProtBAG_z are read directly from the
+# resilience phenotype TSV.
+#
+# IMPORTANT FOR T1:
+# If T1_MUSE_GM.tsv contains a total intracranial volume / head-size variable,
+# add its exact column name to IDP_COVARIATES below, e.g.
+#
+#   IDP_COVARIATES="your_exact_ICV_column"
+#
+# Do not invent an ICV column name; leave this empty unless the exact variable
+# exists in T1_MUSE_GM.tsv.
+# -----------------------------------------------------------------------------
+
+PHENOTYPE_COVARIATES="age_at_imaging,sex,Brain_ProtBAG_z"
+
+# Optional general covariates read from COV_TSV.
+COVARIATES=""
+
+# Optional T1-specific covariates read from IDP_TSV.
+IDP_COVARIATES=""
+
+# Categorical covariates included in the model.
+FACTOR_COVARIATES="sex"
+
+# -----------------------------------------------------------------------------
+# QC
+# -----------------------------------------------------------------------------
+
+OUTLIER_SD="4"
+MIN_TOTAL_N="100"
+MIN_PER_GROUP="30"
+
+# -----------------------------------------------------------------------------
+# R environment
+# -----------------------------------------------------------------------------
+
+module load R/4.2.2
+
+# -----------------------------------------------------------------------------
+# Run one T1 IDP
+# -----------------------------------------------------------------------------
+
+comparison="${CASE_LABEL}_vs_${CONTROL_LABEL}"
+output_file="${OUTPUT_DIR}/${comparison}_logistic_results_${idp}.tsv"
+
+echo "============================================================"
+echo "CRA/CUA T1 gray-matter association"
+echo "============================================================"
+echo "SLURM task:       ${SLURM_ARRAY_TASK_ID} / $((${#idp_array[@]} - 1))"
+echo "IDP:              ${idp}"
+echo "Phenotype column: ${PHENOTYPE_COL}"
+echo "Case:             ${CASE_LABEL} = 1"
+echo "Control:          ${CONTROL_LABEL} = 0"
+echo "Output:           ${output_file}"
+echo "============================================================"
+
+if [[ -s "${output_file}" ]]; then
+  echo "Result already exists; skipping:"
+  echo "  ${output_file}"
+  exit 0
+fi
+
+Rscript "${R_SCRIPT}" \
+  "${PHENOTYPE_TSV}" \
+  "${IDP_TSV}" \
+  "${COV_TSV}" \
+  "${OUTPUT_DIR}" \
+  "${idp}" \
+  "${PHENOTYPE_COL}" \
+  "${CASE_LABEL}" \
+  "${CONTROL_LABEL}" \
+  "${COVARIATES}" \
+  "${FACTOR_COVARIATES}" \
+  "${PHENOTYPE_COVARIATES}" \
+  "${IDP_COVARIATES}" \
+  "${OUTLIER_SD}" \
+  "${MIN_TOTAL_N}" \
+  "${MIN_PER_GROUP}"
+
+echo "Finished:"
+echo "  ${idp}"
